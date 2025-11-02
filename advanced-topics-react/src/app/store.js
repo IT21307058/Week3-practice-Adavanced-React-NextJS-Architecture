782e7d3bb9aacb6f7906cbcfa0ab122da63e5ef0
Bhanuka Lakshitha Dayananda <99331889+IT21307058@users.noreply.github.com>
2025-11-02 21:11:47 +0530

    changes commited

--- advanced-topics-react/src/app/store.js
+++ advanced-topics-react/src/app/store.js
@@ -5,10 +5,27 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
     reducer: {
+        //         In my Redux store, I want two pieces of state:
+        // one for my counter, and one for my API data
         counter: counterReducer,
         [dummyDataApi.reducerPath]: dummyDataApi.reducer,
     },
 
+    //Middleware = special helpers that sit between your actions and reducers.
+    //Now, that API needs something to:
+
+// Actually send the network request (fetch)
+
+// Store and cache the response
+
+// Handle background refetches
+
+// Manage loading / error states
+
+// Keep data fresh when you focus the tab again
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(dummyDataApi.middleware),
 })
+
+//Enables auto-refetching features
+setupListeners(store.dispatch);
